6fb7b54257ad1e7e2efe0dc6375ec39e9f875f3f
Kashish352 <kashish2k201@gmail.com>
2024-12-10 02:51:24 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -1,55 +1,60 @@
-import express from "express";
-import cors from "cors";
-import mongoose from "mongoose";
-import * as dotenv from "dotenv";
-import UserRouter from "./routes/user.js";
-import OrderRouter from "./routes/orders.js";
+import axios from "axios";
 
-dotenv.config();
-
-const app = express();
-app.use(cors());
-app.use(express.json({ limit: "50mb" }));
-app.use(express.urlencoded({ extended: true }));
-
-//error handel
-app.use((err, req, res, next) => {
-  const status = err.status || 500;
-  const message = err.message || "Something went wrong";
-  return res.status(status).json({
-    success: false,
-    status,
-    message,
-  });
+const API = axios.create({
+  baseURL: "http://localhost:8080/api/",
 });
 
-app.get("/", async (req, res) => {
-  res.status(200).json({
-    message: "Hello This is Group 1 Project",
+export const UserSignUp = async (data) => await API.post("/user/signup", data);
+export const UserSignIn = async (data) => await API.post("/user/signin", data);
+
+//Products
+export const getAllProducts = async (filter) =>
+  await API.get(`/products?${filter}`);
+
+export const getProductDetails = async (id) => await API.get(`/products/${id}`);
+
+//Cart
+
+export const getCart = async (token) =>
+  await API.get("/user/cart", {
+    headers: { Authorization: `Bearer ${token}` },
   });
-});
 
-app.use("/api/user/", UserRouter);
+export const addToCart = async (token, data) =>
+  await API.post(`/user/cart/`, data, {
+    headers: { Authorization: `Bearer ${token}` },
+  });
 
-app.use("/api/orders", OrderRouter);
-const connectDB = () => {
-  mongoose.set("strictQuery", true);
-  mongoose
-    .connect(process.env.MODNO_DB)
-    .then(() => console.log("Connected to MONGO DB"))
-    .catch((err) => {
-      console.error("failed to connect with mongo");
-      console.error(err);
-    });
-};
+export const deleteFromCart = async (token, data) =>
+  await API.patch(`/user/cart/`, data, {
+    headers: { Authorization: `Bearer ${token}` },
+  });
 
-const startServer = async () => {
-  try {
-    connectDB();
-    app.listen(8080, () => console.log("Server started on port 8080"));
-  } catch (error) {
-    console.log(error);
-  }
-};
+//Favourites
 
-startServer();
+export const getFavourite = async (token) =>
+  await API.get(`/user/favorite`, {
+    headers: { Authorization: `Bearer ${token}` },
+  });
+
+export const addToFavourite = async (token, data) =>
+  await API.post(`/user/favorite/`, data, {
+    headers: { Authorization: `Bearer ${token}` },
+  });
+
+export const deleteFromFavourite = async (token, data) =>
+  await API.patch(`/user/favorite/`, data, {
+    headers: { Authorization: `Bearer ${token}` },
+  });
+
+//Orders
+
+export const placeOrder = async (token, data) =>
+  await API.post(`/user/order/`, data, {
+    headers: { Authorization: `Bearer ${token}` },
+  });
+
+export const getOrders = async (token) =>
+  await API.get(`/user/order/`, {
+    headers: { Authorization: `Bearer ${token}` },
+  });
